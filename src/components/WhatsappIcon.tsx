4fee5d3a5ef48cea2a68e9b7b92b9697c61f8e31
buywebteam <buywebteam@gmail.com>
2025-06-01 23:05:39 +0100

    Update WhatsappIcon.tsx

--- src/components/WhatsappIcon.tsx
+++ src/components/WhatsappIcon.tsx
@@ -3,7 +3,7 @@ import { PiTelegramLogoThin } from "react-icons/pi";
 const StickyWhatsappIcon = () => {
   return (
     <a
-      href="https://t.me/+2348116074956"
+      href="https://t.me/@customercareadmin0"
       target="_blank"
       rel="noopener noreferrer"
       className="fixed bottom-5 right-5 bg-blue-500 text-white p-3 rounded-full shadow-lg hover:bg-blue-700 transition duration-300 ease-in-out"
